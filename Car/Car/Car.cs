230d3b18d9e43827e6edfdc95eb09b004f175d03
Bundschuh, Jonas <jonas.bundschuh@tecalliance.net>
2022-09-21 14:38:44 +0200

    v0.1

---
+++ Car/Car/Car.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+namespace Car
+{
+    public class Car
+    {
+
+        public double TypeId { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string TypeName { get; set; }
+        public string Farbe { get; set; }
+        public int Geschwindigkeit { get; set; }
+        public bool MotorAn { get; set; }
+        public bool Speed { get; set; }
+        public bool Bremsen { get; set; }
+        public bool ChangeColor { get; set; }
+
+
+        public Car(double typeid, string make, string model, string typename, string farbe, int geschwindigkeit, bool motorAn, bool speed, bool bremsen, bool changeColor)
+        {
+            TypeId = typeid;
+            Make = make;
+            Model = model;
+            TypeName = typename;
+            Farbe = farbe;
+            Geschwindigkeit = geschwindigkeit;
+            MotorAn = motorAn;
+            Speed = speed;
+            Bremsen = bremsen;
+            ChangeColor = changeColor;
+
+
+
+        }
+
+        public void PrintCar()
+        {
+            Console.WriteLine($"Car {TypeId} starts. The car has the color {Farbe} and drives at a speed of {Geschwindigkeit}");
+        }
+
+        public void Start()
+        {
+            if (MotorAn == true)
+            {
+                Console.WriteLine("Das auto ist bereits an!");
+            }
+            else
+            {
+                Console.WriteLine("Auto startet \nBRUMMMM");
+                MotorAn = true;
+            }
+
+        }
+        public void SpeedSTart()
+        {
+            if (Speed == true)
+            {
+                Console.WriteLine("Noch schneller geht nicht!");
+            }
+            else
+            {
+                Console.WriteLine("beschleunige! \nWRUUUM!");
+            }
+        }
+
+        public void BemsenStart()
+        {
+            if(Bremsen == true)
+            {
+                Console.WriteLine("Wird schon gebremst!");
+            }
+            else
+            {
+                Console.WriteLine("BREMSEN!!!!! \nQuiiiiiiiiiiiitsch");
+            }
+        }
+
+        public void LackierenStart()
+        {
+            if(ChangeColor == true)
+            {
+                Console.WriteLine("What do you want to change the color of your vehicle to?");
+                string newColor = Console.ReadLine();
+                if (newColor == Farbe)
+                {
+                    Console.WriteLine($"Die Farbe ist bereits {Farbe}.");
+                }
+                else
+                {
+                    Farbe = newColor;
+                    Console.WriteLine($"Die neue Farbe deines Autos ist jetzt {Farbe}"); 
+                }
+            }
+        }
+
+
+
+
+
+
+
+
+    }
+}
--- Car/Car/Program.cs
+++ Car/Car/Program.cs
@@ -10,6 +10,43 @@ namespace Car
     {
         static void Main(string[] args)
         {
+            
+            List<Car> list = new List<Car>();   
+            list.Add(new Car(1337, "BMW", "i8", "BMW", "White", 500, false, false, false, true));
+            
+            foreach (Car c in list)
+            {
+
+                Console.WriteLine("Would you like to start? (y/n)");
+                string userStart = Console.ReadLine();
+
+                if(userStart == "y")
+                {
+                    Console.Clear();
+                    c.PrintCar();
+                    c.LackierenStart();
+                    Console.Clear();
+                    c.PrintCar();
+                    c.SpeedSTart();
+                    c.SpeedSTart();
+                    c.BemsenStart();
+                    
+                }
+                else if(userStart == "n")
+                {
+                    Console.WriteLine("Good Bye");
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("not a valid option!");
+                }
+
+           
+               
+            }
+
+            Console.ReadLine();
         }
     }
 }
